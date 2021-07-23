37aa8ba6f3f6c497abec7ae54c3ea51f8111258f
RebaiHamza <rebai.hamza93@gmail.com>
2021-07-23 11:23:09 +0100

    v12.2

--- src/pages/admin/AssiduiteAdmin/AssiduiteAdmin.js
+++ src/pages/admin/AssiduiteAdmin/AssiduiteAdmin.js
@@ -1,9 +1,4 @@
 import React, { useState, useEffect } from "react"; 
-import { Grid, Table,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell, } from "@material-ui/core";
 import MUIDataTable from "mui-datatables";
 import useStyles from "./styles";
 import { withStyles, useTheme } from '@material-ui/core/styles';
@@ -18,8 +13,42 @@ import { Link,Button, Avatar } from "../../../components/Wrappers/Wrappers";
 import axios from "axios";
 import * as moment from 'moment'
 import { CircularProgress } from "../../../components/Wrappers/Wrappers";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 // const [mat, setmat] =  
+import {
+  Grid,
+  Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Paper
+} from "@material-ui/core";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "OPEN_GRID":
+      return {
+        ...state,
+        toggleGrid: true
+      };
+    case "CLOSE_GRID":
+      return {
+        ...state,
+        toggleGrid: false
+      };
+    }
+  };
 export default function Assuidite() {
+  const [state, dispatch] = React.useReducer(reducer, {
+    toggleModal: false,
+    toggleBody: false,
+    toggleSmall: false,
+    toggleGrid: false,
+    toggleLarge: false,
+    toggleInputModal: false
+  });
   const classes = useStyles();
   const [CoursM, seCoursM] = useState([]);
   useEffect(function () {
@@ -36,14 +65,27 @@ export default function Assuidite() {
   }, []);
   const [mat, setmat] =useState([]);
   function  reg  ( id ) {
- 
    const d= sessionStorage.getItem('user_id')
     axios
-       
       .get(`http://www.pointofsaleseedigitalaency.xyz/public/APIUser/AssiduteAdmin/${id}`)
       .then(res => {
      setmat(res.data)
-     
+      }, 2000)
+  } 
+
+
+
+
+  const [mats, setmats] =useState([]);
+  function  regs  ( id ) {
+   const d= sessionStorage.getItem('user_id')
+
+   dispatch({ type: "OPEN_GRID" })
+    axios
+      .get(`http://www.pointofsaleseedigitalaency.xyz/public/APIUser/NbrAbsence/${id}`)
+      .then(res => {
+     setmats(res.data)
+    
       }, 2000)
   } 
   return (
@@ -57,7 +99,6 @@ export default function Assuidite() {
      variant="contained"
       onClick={()=>{reg("1")}} 
    > Eleves
- 
    </Button>
    <Button style={{backgroundColor: "#0E0D47",width:'150px'}}
      color="primary"
@@ -70,15 +111,13 @@ export default function Assuidite() {
      color="primary"
      variant="contained"
      onClick={()=>{reg("3")}} 
-   > Agents administratifs
- 
+   > 
+   Agents administratifs
    </Button>
-      
          </div>
          <br />
          <br />
          <br />
-     
 <Grid container spacing={4}>
         <Grid item xs={12}>
           <MUIDataTable
@@ -90,16 +129,24 @@ export default function Assuidite() {
               options: {
                   customBodyRender: (value, tableMeta, updateValue) => {
                       return (
-                        
-
-                        <Button style={{backgroundColor: "#0E0D47" }} onClick={() => console.log( tableMeta.rowData["0"]) }>
-                        Edit
-                        </Button>
+                        <Button
+                      
+                       
+                        className={classes.marginRight}
+                        onClick={
+                          () => regs(tableMeta.rowData["0"]) }
+                      
+                       
+                      >
+                       < MoreVertIcon/>
+                      </Button>
+                        // <Button style={{backgroundColor: "#0E0D47" }} onClick={() => console.log( tableMeta.rowData["0"]) }>
+                        // Edit
+                        // </Button>
                       )
                   }
               }
           }
-          
           ]}
             options={{
               filterType: "checkbox"
@@ -107,22 +154,46 @@ export default function Assuidite() {
           />
         </Grid>
       </Grid>
-      
+      <Dialog
+              fullWidth={true}
+              maxWidth={"lg"}
+              open={state.toggleGrid}
+              onClose={() => dispatch({ type: "CLOSE_GRID" })}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">{"Absence"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText
+                  id="alert-dialog-description"
+                  component={"div"}
+                >
+
+<MUIDataTable
+            title="Gestion des Congés"
+            data={ mats }
+            columns={[    "nombre absence",
+            "date_absence"
+          ]}
+            options={{
+              filterType: "checkbox"
+            }}
+          />
+
+                  
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button
+                  onClick={() => dispatch({ type: "CLOSE_GRID" })}
+                  color="primary"
+                >
+                    Fermer
+                </Button>
+                
+              </DialogActions>
+            </Dialog>
           </div>
           </div>
   );
 }
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-
